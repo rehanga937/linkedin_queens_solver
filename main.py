1764268a90fbf369608143b60a465dcbfb1a8567
Rehanga Gamage <RehangaG@brownsgroup.com>
2025-08-29 11:50:32 +0530

    Commented out first narrowing down logic, as I will reimplement it later.
    The logic:
    'If a row's (or column's) remaining blank cells are all of the same color, then cross of all the other cells of the same color'.
    ...This can be applied to n rows (or columns) containing n colors.

--- main.py
+++ main.py
@@ -26,32 +26,32 @@ while True:
 
     # narrowing down logic
     ## If a row's (or column's) remaining blank cells are all of the same color, then cross of all the other cells of the same color
-    for row_y, row in enumerate(board.cell_grid):
-        unique_colors = set()
-        for cell in row:
-            if cell.status == CellStatus.BLANK:
-                unique_colors.add(cell.color)
-        if len(unique_colors) == 1:
-            color_set = board.color_sets[unique_colors.pop()]
-            if color_set.held_rows == {row_y}: continue # skip the turn stuff below
-            color_set.only_keep_one_axis(row_y, 'row')
-            print(f"Turn {turn}: Row {row_y} with only same color")
-            board.to_excel(f"{FILEPATH}.xlsx", turn)
-            turn += 1
+    # for row_y, row in enumerate(board.cell_grid):
+    #     unique_colors = set()
+    #     for cell in row:
+    #         if cell.status == CellStatus.BLANK:
+    #             unique_colors.add(cell.color)
+    #     if len(unique_colors) == 1:
+    #         color_set = board.color_sets[unique_colors.pop()]
+    #         if color_set.held_rows == {row_y}: continue # skip the turn stuff below
+    #         color_set.only_keep_one_axis(row_y, 'row')
+    #         print(f"Turn {turn}: Row {row_y} with only same color")
+    #         board.to_excel(f"{FILEPATH}.xlsx", turn)
+    #         turn += 1
     
-    for col_x in range(0, board.length):
-        unique_colors = set()
-        for row_y in range(0, board.height):
-            cell = board.cell_grid[row_y][col_x]
-            if cell.status == CellStatus.BLANK:
-                unique_colors.add(cell.color)
-        if len(unique_colors) == 1:
-            color_set = board.color_sets[unique_colors.pop()]
-            if color_set.held_cols == {col_x}: continue # skip the turn stuff below
-            color_set.only_keep_one_axis(col_x, 'col')
-            print(f"Turn {turn}: Column {col_x} with only same color")
-            board.to_excel(f"{FILEPATH}.xlsx", turn)
-            turn += 1
+    # for col_x in range(0, board.length):
+    #     unique_colors = set()
+    #     for row_y in range(0, board.height):
+    #         cell = board.cell_grid[row_y][col_x]
+    #         if cell.status == CellStatus.BLANK:
+    #             unique_colors.add(cell.color)
+    #     if len(unique_colors) == 1:
+    #         color_set = board.color_sets[unique_colors.pop()]
+    #         if color_set.held_cols == {col_x}: continue # skip the turn stuff below
+    #         color_set.only_keep_one_axis(col_x, 'col')
+    #         print(f"Turn {turn}: Column {col_x} with only same color")
+    #         board.to_excel(f"{FILEPATH}.xlsx", turn)
+    #         turn += 1
 
     ## cross off cells that if were queens, would block other color sets
     blank_cells = board.get_blank_cells()
